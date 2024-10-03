277176667cff1101136dadbfec46793b4cadc40c
Neel Islam <rabiul17.ri@gmail.com>
2024-10-03 10:12:07 +0600

    update 7

--- Incapsulation/src/Encapsulation.java
+++ Incapsulation/src/Encapsulation.java
@@ -1,5 +1,8 @@
 public class Encapsulation {
     public static void main(String[] args) {
+        Person1 r = new Person1();
+        r.setName("John");
 
+        String name = r.getName();
     }
 }
---
+++ Incapsulation/src/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+
+
+    Person1 person = new Person1();
+}
+
+class Person1 {
+
+    private String name;
+    private int id;
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+
+
+}
