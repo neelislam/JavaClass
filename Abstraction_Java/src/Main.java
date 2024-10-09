b443a51709243785055b0db680b63025a36af58b
Neel Islam <rabiul17.ri@gmail.com>
2024-10-09 11:20:33 +0600

    update 11

--- Abstraction_Java/src/Main.java
+++ Abstraction_Java/src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+// keep the element secret but not the functionality
     }
 }
